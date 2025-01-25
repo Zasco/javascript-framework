56946fc2202639d4e1401ed1033d2e597a28fafe
Zasco <zasco_0@protonmail.com>
2025-01-25 13:27:02 -0500

    Add console logger

---
+++ entity/ConsoleLogger.js
@@ -0,0 +1,12 @@
+// @ts-check
+import Logger from '../interface/Logger.js';
+
+export default class ConsoleLogger extends Logger {
+    /**
+     * @inheritdoc
+     * @param {*} message The message to log.
+     */
+    log(message) {
+        console.log(message);
+    }
+}
