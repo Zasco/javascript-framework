9b03f08ae8f75067e7645548139fb2c214a03014
Zasco <zasco_0@protonmail.com>
2025-01-25 13:28:10 -0500

    Added package index

---
+++ index.js
@@ -0,0 +1,11 @@
+import ErrorHandler from './ErrorHandler.js';
+import LogHelper from './LogHelper.js';
+import Logger from './interface/Logger.js';
+import ConsoleLogger from './entity/ConsoleLogger.js';
+
+export {
+    ErrorHandler,
+    LogHelper,
+    Logger,
+    ConsoleLogger,
+};
