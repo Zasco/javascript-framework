cc8c6c470cb1c1b70780cefdc037ed9c3f708736
Zasco <zasco_0@protonmail.com>
2025-02-06 07:23:17 -0500

    Improve index.js organisation
    Add comments and improve modules grouping.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import ErrorHandler from './utils/ErrorHandler.js';
 import LogHelper from './utils/LogHelper.js';
+
 import Logger from './interface/Logger.js';
 import ConsoleLogger from './entity/ConsoleLogger.js';
 
 export {
+    // Helpers & Utils
     ErrorHandler,
     LogHelper,
+
+    // Interfaces
     Logger,
+
+    // Entities
     ConsoleLogger,
 };
