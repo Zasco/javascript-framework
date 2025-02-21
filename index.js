fa63ddf5216d93a1cd03883bd51fc85bd051cacb
Zasco <zasco_0@protonmail.com>
2025-02-20 19:30:40 -0500

    style(index): add missing space between imports

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import ErrorUtils from './src/utils/ErrorUtils.js';
 import LogHelper from './src/utils/LogHelper.js';
 
 import Logger from './src/interfaces/Logger.js';
+
 import ConsoleLogger from './src/entities/ConsoleLogger.js';
 
 /** @typedef {import('./src/config/LogLevelsConfig.js').LogLevel} LogLevel */
