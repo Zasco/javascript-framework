f51d275a5f04a9a5072b8b4da62a725248d49792
Zasco <zasco_0@protonmail.com>
2025-02-21 17:39:36 -0500

    feat(ConsoleLogger): add log level support
    Add `getLogFunctionForLogLevel()` method to map `LOG_LEVELS` to corresponding console logging function.
    Import `LOG_LEVELS` configuration and use `LogLevel` type definition.

--- src/entities/ConsoleLogger.js
+++ src/entities/ConsoleLogger.js
@@ -1,10 +1,41 @@
 import Logger from '../interfaces/Logger.js';
 
+import LOG_LEVELS from '../config/LogLevelsConfig.js';
+
+/** @typedef {import('../config/LogLevelsConfig.js').LogLevel} LogLevel */
+
 /**
  * @since 0.0.1
  */
 export default class ConsoleLogger extends Logger {
     log(message) {
         console.log(message);
+    };
+
+    /**
+     * Returns the log function for a given log level.
+     * 
+     * @since ${NEXT_VERSION}
+     * @param {LogLevel} level
+     * @returns {function}
+     */
+    getLogFunctionForLogLevel(level) {
+        switch (level) {
+            case LOG_LEVELS.DEBUG:
+                return console.debug;
+
+            case LOG_LEVELS.INFO:
+                return console.info;
+
+            case LOG_LEVELS.WARNING:
+                return console.warn;
+
+            case LOG_LEVELS.ERROR:
+                return console.error;
+                
+            case LOG_LEVELS.LOG:
+            default:
+                return console.log;
+        }
     };
 }
