54b2aea684981aaa705ba21efe1eb33208815236
Zasco <zasco_0@protonmail.com>
2025-02-22 16:07:18 -0500

    feat: add error levels configuration

---
+++ src/config/ErrorLevelsConfig.js
@@ -0,0 +1,14 @@
+/** 
+ * @since ${NEXT_VERSION}
+ * @typedef {number} ErrorLevel
+ * @typedef {{[ErrorLabel: string]: ErrorLevel}} ErrorLevelsConfig
+ */
+
+/** 
+ * @since ${NEXT_VERSION}
+ * @type {ErrorLevelsConfig} 
+ */
+export default {
+    WARNING: 1,
+    ERROR: 2,
+};
