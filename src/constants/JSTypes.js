1e4d2599e006ab248bf50678403c15b789916271
Zasco <zasco_0@protonmail.com>
2025-03-07 17:53:52 -0500

    feat: add JavaScript types constants
    Add constants for all JavaScript primitive and structural types with
    dynamic type derivation instead of hardcoded strings.

---
+++ src/constants/JSTypes.js
@@ -0,0 +1,27 @@
+/**
+ * Constants representing JavaScript data types with values dynamically derived using `typeof` operations.
+ * 
+ * @since ${NEXT_VERSION}
+ * @type {{
+*   'STRING': string, 
+*   'NUMBER': string, 
+*   'BOOLEAN': string, 
+*   'OBJECT': string, 
+*   'UNDEFINED': string, 
+*   'FUNCTION': string, 
+*   'SYMBOL': string, 
+*   'BIGINT': string, 
+* }}
+*/
+const JSTYPES = {
+   STRING: typeof String(), 
+   NUMBER: typeof Number(), 
+   BOOLEAN: typeof Boolean(), 
+   OBJECT: typeof {}, 
+   UNDEFINED: typeof undefined, 
+   FUNCTION: typeof function(){}, 
+   SYMBOL: typeof Symbol(), 
+   BIGINT: typeof BigInt(0), 
+};
+
+export default Object.freeze(JSTYPES);
