ab88b5fd1ebaaa33dc6fc1fc1f19857cda899333
Zasco <zasco_0@protonmail.com>
2025-03-11 07:19:43 -0400

    refactor!: remove splitted Commit properties

--- src/models/Commit.js
+++ src/models/Commit.js
@@ -1,17 +1,3 @@
-import CommitMsg from './CommitMsg.js';
-import SubjectLine from './CommitMsgSubjectLine.js';
-
-/**
- * @since 0.0.3
- * @typedef {string | undefined} optionalStringComponent A string component of the {@link CommitMsg} that is optional.
- * 
- * @typedef {boolean | undefined} isBreakingChange A flag indicating whether the {@link Commit} is a breaking change or not. `undefined` for neither `true` nor `false`.
- * @typedef {optionalStringComponent} CommitMsgContentComponent A component of the {@link CommitMsgContent}.
- * @typedef {optionalStringComponent} CommitMsgBody The body of the {@link CommitMsg}.
- * @typedef {optionalStringComponent} CommitMsgFooter The footer of the {@link CommitMsg}.
- * @typedef {CommitMsgBody & CommitMsgFooter} CommitMsgContent The content of the {@link CommitMsg}.
- */
-
 /**
  * @since 0.0.3
  * @typedef {{
@@ -74,35 +60,5 @@ export default class Commit {
      */
     get commitDate() {
         return this._commitDate;
-    }
-
-    /**
-     * The {@link SubjectLine.type} of the commit.
-     * 
-     * @since 0.0.3
-     * @readonly
-     */
-    get type() {
-        return this._msg.subjectLine.type;
-    }
-
-    /**
-     * If the commit {@link isBreakingChange}.
-     * 
-     * @since 0.0.3
-     * @readonly
-     */
-    get isBreakingChange() {
-        return this._msg.subjectLine.isBreakingChange;
-    }
-
-    /**
-     * Returns the {@link SubjectLine.subject} of the commit.
-     * 
-     * @since 0.0.3
-     * @readonly
-     */
-    get subject() {
-        return this._msg.subjectLine.subject;
     }
 }
