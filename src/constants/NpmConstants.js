087806254290bd4b146696a9dfb5596236bf6e82
Zasco <zasco_0@protonmail.com>
2025-03-14 11:52:55 -0400

    feat: add npm version bump types constant

---
+++ src/constants/NpmConstants.js
@@ -0,0 +1,9 @@
+/** @typedef {import('../types/npm-types.js').VersionBumpTypes} VersionBumpTypes */
+
+/**
+ * @since {NEXT_VERSION}
+ * @see {@link VersionBumpTypes}
+ * @readonly
+ * @type {VersionBumpTypes}
+ */
+export const VERSION_BUMP_TYPES = Object.freeze(['major', 'minor', 'patch']);
