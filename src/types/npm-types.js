6993029dab846464a51226381ebf2a3e586fe38a
Zasco <zasco_0@protonmail.com>
2025-03-14 11:43:14 -0400

    feat: add npm version bump types

---
+++ src/types/npm-types.js
@@ -0,0 +1,5 @@
+/**
+ * @since {NEXT_VERSION}
+ * @typedef {readonly ['major', 'minor', 'patch']} VersionBumpTypes The allowed version bump types for the `npm version` command.
+ * @typedef {VersionBumpTypes[number]} VersionBumpType A single allowed version bump type.
+ */
