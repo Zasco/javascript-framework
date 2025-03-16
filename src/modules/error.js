33301d068e015589fe8f014d8dc07a58811104c1
Zasco <zasco_0@protonmail.com>
2025-03-16 16:35:51 -0400

    refactor!: export level values not constants

--- src/modules/error.js
+++ src/modules/error.js
@@ -1,11 +1,11 @@
 import * as Types from '../config/ErrorLevelsConfig.js';
-import * as Constants from '../config/ErrorLevelsConfig.js';
+import ERROR_LEVELS from '../config/ErrorLevelsConfig.js';
 import Handler from '../helpers/ErrorHandler.js';
 import Utils from '../utils/ErrorUtils.js';
 
 export { 
     Types, 
-    Constants, 
+    ERROR_LEVELS, 
     Handler, 
     Utils, 
 }
--- src/modules/log.js
+++ src/modules/log.js
@@ -1,12 +1,12 @@
 import * as Types from '../config/LogLevelsConfig.js';
-import * as Constants from '../config/LogLevelsConfig.js';
+import LOG_LEVELS from '../config/LogLevelsConfig.js';
 import Helper from '../helpers/LogHelper.js';
 import Logger from '../interfaces/Logger.js';
 import ConsoleLogger from '../entities/ConsoleLogger.js';
 
 export { 
     Types, 
-    Constants, 
+    LOG_LEVELS, 
     Helper, 
     Logger, 
     ConsoleLogger, 
