6fb66b9c1ccfe96a3102213d64d255564601c63c
Zasco <zasco_0@protonmail.com>
2025-03-16 19:40:42 -0400

    feat: add core module with traits, types & consts

---
+++ src/modules/core.js
@@ -0,0 +1,25 @@
+import AbstractClassTrait from '../traits/AbstractClassTrait.js';
+import SingletonTrait from '../traits/SingletonTrait.js';
+
+import * as expandedTypes from '../constants/expanded-types.js';
+import JS_TYPES from '../constants/js-types.js';
+import EXPANDED_TYPES from '../constants/expanded-types.js';
+import TypeUtils from '../utils/TypeUtils.js';
+
+
+// [NOTE] Here will also be the anticipated StringUtils.
+export { JS_TYPES, EXPANDED_TYPES, TypeUtils };
+
+export const types = {
+    expandedTypes, 
+}
+
+export const constants = {
+  JS_TYPES,
+  EXPANDED_TYPES
+}
+
+export const traits = {
+  AbstractClassTrait,
+  SingletonTrait
+}
