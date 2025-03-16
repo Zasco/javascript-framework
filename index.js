e8d710dad451cbff288c59115323654e3ddc4384
Zasco <zasco_0@protonmail.com>
2025-03-16 16:31:03 -0400

    refactor!: remove individual exports from index

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-// Types
-import * as PackageJsonTypes from './src/types/package-json-types.js';
-import * as LogLevelsTypes from './src/config/LogLevelsConfig.js';
-
-// Config
-import LOG_LEVELS from './src/config/LogLevelsConfig.js';
-
-// Interfaces
-import Logger from './src/interfaces/Logger.js';
-
 // Traits
 import AbstractClassTrait from './src/traits/AbstractClassTrait.js';
 import SingletonTrait from './src/traits/SingletonTrait.js';
-
-// Models
-import FileSystemPath from './src/models/FileSystemPath.js';
-import Action from './src/models/Action.js';
-import Repository from './src/models/Repository.js';
-import Commit from './src/models/Commit.js';
-
-// Utils
-import ErrorUtils from './src/utils/ErrorUtils.js';
-import TypeUtils from './src/utils/TypeUtils.js';
-
-// Helpers
-import ErrorHandler from './src/helpers/ErrorHandler.js';
-import LogHelper from './src/helpers/LogHelper.js';
-import GitHelper from './src/helpers/GitHelper.js';
-import PackageJsonHelper from './src/helpers/PackageJsonHelper.js';
-
-// Entities
-import ConsoleLogger from './src/entities/ConsoleLogger.js';
 
 // Modules
 import * as typesModule from './src/modules/types.js';
@@ -49,37 +20,9 @@ import * as actionModule from './src/modules/action.js';
  */
 
 export {
-    // Types
-    PackageJsonTypes, 
-    LogLevelsTypes, 
-    
-    // Config
-    
-    // Interfaces
-    Logger, 
-    
     // Traits
     AbstractClassTrait, 
     SingletonTrait, 
-
-    // Models
-    FileSystemPath, 
-    Action, 
-    Repository, 
-    Commit, 
-    
-    // Utils
-    ErrorUtils, 
-    TypeUtils, 
-    
-    // Helpers
-    ErrorHandler, 
-    LogHelper, 
-    GitHelper, 
-    PackageJsonHelper, 
-
-    // Entities
-    ConsoleLogger, 
 
     // Modules
     typesModule, 
