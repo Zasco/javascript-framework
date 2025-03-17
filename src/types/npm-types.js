48ed9f52715f41f75fd34d49ab59f192eeebcea1
Zasco <zasco_0@protonmail.com>
2025-03-17 17:42:15 -0400

    feat: add new bump types to npm version

--- src/types/npm-types.js
+++ src/types/npm-types.js
@@ -1,5 +1,20 @@
 /**
  * @since {NEXT_VERSION}
- * @typedef {readonly ['major', 'minor', 'patch']} VersionBumpTypes The allowed version bump types for the `npm version` command.
- * @typedef {VersionBumpTypes[number]} VersionBumpType A single allowed version bump type.
+ * @typedef {readonly [
+ *  'major', 
+ *  'minor', 
+ *  'patch', 
+ *  'premajor', 
+ *  'preminor', 
+ *  'prepatch', 
+ *  'prerelease', 
+ *  'from-git', 
+ * ]} VersionBumpTypes
+ */
+
+/**
+ * A single version bump type.
+ * 
+ * @since ${NEXT_VERSION}
+ * @typedef {VersionBumpTypes[number]} VersionBumpType
  */
