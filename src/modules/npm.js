11f82166c0da40295d33d68baaefca22d0154af9
Zasco <zasco_0@protonmail.com>
2025-03-17 17:24:07 -0400

    refactor: improve default re-export in npm module

--- src/modules/npm.js
+++ src/modules/npm.js
@@ -1,6 +1,4 @@
-import NpmCliWrapper from '../wrappers/NpmCliWrapper.js';
-
 export * as types from '../types/npm-types.js';
 export * as constants from '../constants/npm-constants.js';
 
-export const executeNpmVersion = NpmCliWrapper.executeNpmVersion.bind(NpmCliWrapper);
+export { default } from '../wrappers/NpmCliWrapper.js';
