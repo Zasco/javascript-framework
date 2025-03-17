9982e2c6ce1d418423e0eeaab993212df5082fe5
Zasco <zasco_0@protonmail.com>
2025-03-17 17:48:00 -0400

    feat: add npm cli wrapper types

---
+++ src/types/npm-cli-wrapper-types.js
@@ -0,0 +1,8 @@
+import * as commitlintTypes from './npm-types.js';
+
+/**
+ * The options to pass to `version()`.
+ * 
+ * @since ${NEXT_VERSION}
+ * @typedef {Partial<commitlintTypes.VersionOptionTypes>} VersionOptions
+ */
