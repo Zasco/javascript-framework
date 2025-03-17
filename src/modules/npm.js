37a444c7839c0fe8809c7be65fb7455ae4826eac
Zasco <zasco_0@protonmail.com>
2025-03-16 20:47:58 -0400

    fix(npm): export executeNpmVersion() method
    Mistakenly removed in 4734172.
    Remove unused `NpmCliWrapper`.

--- src/modules/npm.js
+++ src/modules/npm.js
@@ -1,3 +1,6 @@
+import NpmCliWrapper from '../wrappers/NpmCliWrapper.js';
+
 export * as types from '../types/npm-types.js';
 export * as constants from '../constants/npm-constants.js';
-export { default as NpmCliWrapper } from '../wrappers/NpmCliWrapper.js';
+
+export const executeNpmVersion = NpmCliWrapper.executeNpmVersion;
