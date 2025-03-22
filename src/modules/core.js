d8dd2c28252f112201717920c0c1d1360e15a420
Zasco <zasco_0@protonmail.com>
2025-03-22 13:46:39 -0400

    feat: export special chars const in core module

--- src/modules/core.js
+++ src/modules/core.js
@@ -2,13 +2,14 @@ import AbstractClassTrait from '../traits/AbstractClassTrait.js';
 import SingletonTrait from '../traits/SingletonTrait.js';
 
 import * as expandedTypes from '../constants/expanded-types.js';
+import * as SPECIAL_CHARS from '../constants/special-chars.js';
 import JS_TYPES from '../constants/js-types.js';
 import EXPANDED_TYPES from '../constants/expanded-types.js';
 import TypeUtils from '../utils/TypeUtils.js';
 
 
 // [NOTE] Here will also be the anticipated StringUtils.
-export { JS_TYPES, EXPANDED_TYPES, TypeUtils };
+export { SPECIAL_CHARS, JS_TYPES, EXPANDED_TYPES, TypeUtils };
 
 export const types = {
     expandedTypes, 
