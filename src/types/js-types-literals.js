139150899c037dec56670321542813253e0ae3ff
Zasco <zasco_0@protonmail.com>
2025-03-22 16:00:48 -0400

    feat: add types literals

---
+++ src/types/js-types-literals.js
@@ -0,0 +1,53 @@
+
+/**
+ * Type definitions for JavaScript type literals.
+ * These types represent the string literals returned by the JavaScript `typeof` operator.
+ * 
+ * @since {NEXT_VERSION}
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'string'} String 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'number'} Number 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'boolean'} Boolean 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'object'} Object 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'undefined'} Undefined 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'function'} Function 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'symbol'} Symbol 
+ */
+
+/** 
+ * @since {NEXT_VERSION} 
+ * @typedef {'bigint'} BigInt 
+ */
+
+/** 
+ * @since {NEXT_VERSION}
+ * @description This is 'object' due to a historical quirk in JavaScript where `typeof null` returns this value.
+ * @typedef {'object'} Null
+ */
