ee8bf5c75d27a303cb538096dc06f1e811ad532a
Zasco <zasco_0@protonmail.com>
2025-03-22 16:10:50 -0400

    refactor: use types literals in constants

--- src/constants/js-types.js
+++ src/constants/js-types.js
@@ -1,17 +1,19 @@
+import * as JsTypesLiterals from '../types/js-types-literals.js';
+
 /**
  * Constants representing JavaScript data types with values dynamically derived using `typeof` operations.
  * 
  * @since 0.0.3
  * @type {{
- *   'STRING': string, 
- *   'NUMBER': string, 
- *   'BOOLEAN': string, 
- *   'OBJECT': string, 
- *   'UNDEFINED': string, 
- *   'FUNCTION': string, 
- *   'SYMBOL': string, 
- *   'BIGINT': string, 
- *   'NULL': string, 
+ *   STRING: JsTypesLiterals.String, 
+ *   NUMBER: JsTypesLiterals.Number, 
+ *   BOOLEAN: JsTypesLiterals.Boolean, 
+ *   OBJECT: JsTypesLiterals.Object, 
+ *   UNDEFINED: JsTypesLiterals.Undefined, 
+ *   FUNCTION: JsTypesLiterals.Function, 
+ *   SYMBOL: JsTypesLiterals.Symbol, 
+ *   BIGINT: JsTypesLiterals.BigInt, 
+ *   NULL: JsTypesLiterals.Null
  * }}
  */
 const JS_TYPES = {
