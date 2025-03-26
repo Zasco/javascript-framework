42c7f9364332e349459a1c3de2c9566d6d0a5f24
Zasco <zasco_0@protonmail.com>
2025-03-25 20:10:55 -0400

    feat: add git constants

---
+++ src/constants/git-constants.js
@@ -0,0 +1,104 @@
+/**
+ * Constants for the `Git` command line tool.
+ * 
+ * @since ${NEXT_VERSION}
+ */
+
+import * as gitTypes from '../types/git-types.js';
+
+
+// Commands
+
+/**
+ * The supported commands for `Git`.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link npmTypes.CommandsType}
+ * @readonly
+ * @type {Object<string, gitTypes.Command>}
+ */
+export const COMMANDS = Object.freeze({
+    VERSION: 'version', 
+    REV_PARSE: 'rev-parse', 
+    DIFF: 'diff', 
+    TAG: 'tag', 
+    ADD: 'add', 
+    COMMIT: 'commit', 
+    PUSH: 'push', 
+});
+
+
+// `add` command (see https://git-scm.com/docs/git-add)
+
+/**
+ * The CLI options for the `add` command.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link gitTypes.AddCliOptions}
+ * @readonly
+ * @type {Object<string, gitTypes.AddCliOption>}
+ */
+export const ADD_OPTIONS = Object.freeze({
+    ALL: 'all', 
+});
+
+
+// `commit` command (see https://git-scm.com/docs/git-commit)
+
+/**
+ * The CLI options for the `commit` command.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link gitTypes.CommitCliOptions}
+ * @readonly
+ * @type {Object<string, gitTypes.CommitCliOption>}
+ */
+export const COMMIT_OPTIONS = Object.freeze({
+    MESSAGE: 'message', 
+});
+
+
+// `diff` command (see https://git-scm.com/docs/git-diff)
+
+/**
+ * The CLI options for the `diff` command.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link gitTypes.DiffCliOptions}
+ * @readonly
+ * @type {Object<string, gitTypes.DiffCliOption>}
+ */
+export const DIFF_OPTIONS = Object.freeze({
+    CACHED: 'cached', 
+    NAME_ONLY: 'name-only', 
+});
+
+
+// `rev-parse` command (see https://git-scm.com/docs/git-rev-parse)
+
+/**
+ * The CLI options for the `rev-parse` command.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link gitTypes.RevParseCliOptions}
+ * @readonly
+ * @type {Object<string, gitTypes.RevParseCliOption>}
+ */
+export const REV_PARSE_OPTIONS = Object.freeze({
+    IS_INSIDE_WORK_TREE: 'is-inside-work-tree', 
+});
+
+
+// `tag` command (see https://git-scm.com/docs/git-tag)
+
+/**
+ * The CLI options for the `tag` command.
+ * 
+ * @since ${NEXT_VERSION}
+ * @see {@link gitTypes.RevParseCliOptions}
+ * @readonly
+ * @type {Object<string, gitTypes.TagCliOption>}
+ */
+export const TAG_OPTIONS = Object.freeze({
+    LIST: 'list', 
+});
