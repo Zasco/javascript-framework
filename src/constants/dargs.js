811b7916feffebd58116f2cb919059392f813014
Zasco <zasco_0@protonmail.com>
2025-03-27 20:07:03 -0400

    feat: add dargs constants

---
+++ src/constants/dargs.js
@@ -0,0 +1,23 @@
+/**
+ * Constants for the `dargs` library.
+ * 
+ * Valid with version `8.1.0`.
+ * 
+ * @since ${NEXT_VERSION}
+ */
+
+import * as dargsTypes from '../types/dargs-types.ts';
+
+/**
+ * @since ${NEXT_VERSION}
+ * @see {@link dargsTypes.PositionalOptionsKey}
+ * @type {dargsTypes.PositionalOptionsKey}
+ */
+export const POSITIONAL_OPTIONS_KEY = '_';
+
+/**
+ * @since ${NEXT_VERSION}
+ * @see {@link dargsTypes.SeparatedOptionsKey}
+ * @type {dargsTypes.SeparatedOptionsKey}
+ */
+export const SEPARATED_OPTIONS_KEY = '--';
