b1699f8d4b75d00c57f582d49df2fc5c3f8f4c28
Zasco <zasco_0@protonmail.com>
2025-03-27 20:09:17 -0400

    feat: export dargs types & constants in cli module

--- src/modules/cli.js
+++ src/modules/cli.js
@@ -1,2 +1,10 @@
 /** @since alpha-5.0.0 */
 export { default as BaseWrapper } from '../models/BaseCliWrapper.js';
+
+
+// dargs
+
+/** @since ${NEXT_VERSION} */
+export * as dargsTypes from '../types/dargs-types.ts';
+/** @since ${NEXT_VERSION} */
+export * as dargsConstants from '../constants/dargs.js';
