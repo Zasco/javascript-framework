faaa8292dbce175c2d5f9b262604ad67f330817b
Zasco <zasco_0@protonmail.com>
2025-03-31 19:21:42 -0400

    chore: deprecate dargs types and constants exports
    Will be removed in `alpha-8.0.0`.

--- src/modules/cli.js
+++ src/modules/cli.js
@@ -1,15 +1,23 @@
 /** @since alpha-5.0.0 */
 export { default as BaseWrapper } from '../models/BaseCliWrapper.js';
 
-
-// dargs
-
-/** @since alpha-7.2.0 */
-export * as dargsTypes from '../types/dargs-types.js';
-/** @since alpha-7.2.0 */
-export * as dargsConstants from '../constants/dargs.js';
-
 /** @since alpha-7.4.0 */
 export * as wrapperTypes from '../types/cli-wrapper-types.js';
 /** @since alpha-7.4.0 */
 export * as wrapperConstants from '../constants/cli-wrapper.js';
+
+
+// Deprecated
+
+import * as rootDargsTypes from '../types/dargs-types.js';
+/**
+ * @since alpha-7.2.0
+ * @deprecated ${NEXT_VERSION}; Will be removed in `alpha-8.0.0`; No replacement.
+ */
+export const dargsTypes = rootDargsTypes;
+import * as rootDargsConstants from '../constants/dargs.js';
+/**
+ * @since alpha-7.2.0
+ * @deprecated ${NEXT_VERSION}; Will be removed in `alpha-8.0.0`; No replacement.
+ */
+export const dargsConstants = rootDargsConstants;
