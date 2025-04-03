2f81dcba9c755d6c6408b3adc952d93b16b11a4d
Zasco <zasco_0@protonmail.com>
2025-04-03 17:16:48 -0400

    feat: add package.json constants

---
+++ src/constants/package-json.js
@@ -0,0 +1,14 @@
+/**
+ * Constants for the `package.json` file.
+ * 
+ * @since ${NEXT_VERSION}
+ */
+
+/** @since ${NEXT_VERSION} */
+export const DEFAULT_FILE_NAME = 'package.json';
+    
+/** @since ${NEXT_VERSION} */
+export const DEFAULT_NAME_KEY = 'name';
+
+/** @since ${NEXT_VERSION} */
+export const DEFAULT_VERSION_KEY = 'version';
