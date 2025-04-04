05d363864a842317b2a602cf9c74efbc4f8860ab
Zasco <zasco_0@protonmail.com>
2025-04-04 19:22:56 -0400

    refactor: move core module exports to sub-files

--- api/core/index.js
+++ api/core/index.js
@@ -1,95 +1,41 @@
-import AbstractClassTrait from '../../src/traits/AbstractClassTrait.js';
-import SingletonTrait from '../../src/traits/SingletonTrait.js';
+import * as types from './types.js';
+import * as constants from './constants.js';
+import * as traits from './traits.js';
+import TypeUtils from '../../src/utils/TypeUtils.js';
 
-import * as SPECIAL_CHARS_CONSTANTS from '../../src/constants/special-chars.js';
-import * as JS_TYPES_CONSTANTS from '../../src/constants/js-types.js';
-import * as EXPANDED_JS_TYPES_CONSTANTS from '../../src/constants/expanded-js-types.js';
+export {
+    /** @since alpha-5.0.0 */
+    types, 
+    /** @since alpha-5.0.0 */
+    constants, 
+    /** @since alpha-5.0.0 */
+    traits, 
+    /** @since alpha-5.0.0 */
+    TypeUtils, 
+};
 
-import TypeUtilsClass from '../../src/utils/TypeUtils.js';
-
-// Types
-
-/** @since alpha-5.0.0 */
-export const types = {
-  /** @since alpha-6.0.0 */
-  expandedJsTypes, 
-  /** @since ${NEXT_VERSION} */
-  utilTypes, 
-  
-
-  // Deprecated
-
-  /**
-   * @since alpha-5.0.0
-   * @deprecated alpha-6.0.0; Will be removed in `alpha-8.0.0`; Use {@link expandedJsTypes} instead
-   */
-  expandedTypes: expandedJsTypes, 
-}
-
-
-// Constants
-
-/** @since alpha-5.0.0 */
-export const constants = {
-  /** @since alpha-5.0.0 */
-  JS_TYPES: JS_TYPES_CONSTANTS,
-  /** @since alpha-6.0.0 */
-  EXPANDED_JS_TYPES: EXPANDED_JS_TYPES_CONSTANTS, 
-
-
-  // Deprecated
-  
-  /**
-   * @since alpha-5.0.0
-   * @deprecated alpha-6.0.0 Use {@link EXPANDED_JS_TYPES_CONSTANTS} instead
-   */
-  EXPANDED_TYPES: EXPANDED_JS_TYPES_CONSTANTS, 
-}
 
 /**
  * @since alpha-6.0.0
  * @see {@link SPECIAL_CHARS_CONSTANTS}
  */
-export const SPECIAL_CHARS = SPECIAL_CHARS_CONSTANTS;
+export const SPECIAL_CHARS = constants.SPECIAL_CHARS;
 
 /**
  * @since alpha-5.0.0
  * @see {@link JS_TYPES_CONSTANTS}
  */
-export const JS_TYPES = JS_TYPES_CONSTANTS;
+export const JS_TYPES = constants.JS_TYPES;
 
 /**
  * @since alpha-6.0.0
  * @see {@link EXPANDED_JS_TYPES_CONSTANTS}
  */
-export const EXPANDED_JS_TYPES = EXPANDED_JS_TYPES_CONSTANTS;
-
-
-// traits
-
-/** @since alpha-5.0.0 */
-export const traits = {
-  /** @since alpha-5.0.0 */
-  AbstractClassTrait, 
-  /** @since alpha-5.0.0 */
-  SingletonTrait, 
-}
-
-
-// Classes
-
-/**
- * @since alpha-5.0.0
- * @see {@link TypeUtilsClass}
- */
-export const TypeUtils = TypeUtilsClass;
-
-
-// Deprecated
+export const EXPANDED_JS_TYPES = constants.EXPANDED_JS_TYPES;
 
 /**
  * @since alpha-5.0.0
  * @deprecated alpha-6.0.0; Will be removed in `alpha-8.0.0`; Use {@link expandedJsTypesConstants} instead
  * @see {@link EXPANDED_JS_TYPES_CONSTANTS}
  */
-export const EXPANDED_TYPES = EXPANDED_JS_TYPES_CONSTANTS;
+export const EXPANDED_TYPES = constants.EXPANDED_JS_TYPES;
