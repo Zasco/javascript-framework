7fd40a00fad1fb59297955730b82c064195b1729
Zasco <zasco_0@protonmail.com>
2025-04-05 06:49:38 -0400

    style: group exports

--- api/action.js
+++ api/action.js
@@ -1,13 +1,16 @@
-import * as actionTypes from '../src/models/Action.js';
-/**
- * @since alpha-5.0.0
- * @see {@link actionTypes}
- */
-export const types = actionTypes;
+import * as types from '../src/models/Action.js';
+import Action from '../src/models/Action.js';
 
-import BaseAction from '../src/models/Action.js';
-/**
- * @since alpha-5.0.0
- * @see {@link BaseAction}
- */
-export const Action = BaseAction;
+export {
+    /**
+     * @since alpha-5.0.0
+     * @see {@link types}
+     */
+    types,
+    
+    /**
+     * @since alpha-5.0.0
+     * @see {@link Action}
+     */
+    Action,
+};
